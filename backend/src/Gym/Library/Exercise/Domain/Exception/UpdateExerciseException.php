ee339d721952aa73a1d7abe9913bcbbd69ba5d7c
Antobio17 <antoniojr997@gmail.com>
2026-07-07 22:21:55 +0200

    feat(exercise): refactor exception handling and validation in Exercise model

--- backend/src/Gym/Library/Exercise/Domain/Exception/UpdateExerciseException.php
+++ backend/src/Gym/Library/Exercise/Domain/Exception/UpdateExerciseException.php
@@ -2,6 +2,7 @@
 
 namespace Gym\Library\Exercise\Domain\Exception;
 
+use Gym\Library\Exercise\Domain\Model\Exercise;
 use Shared\Shared\Shared\Domain\Exception\BaseException;
 
 final class UpdateExerciseException extends BaseException
@@ -12,6 +13,27 @@ final class UpdateExerciseException extends BaseException
             title: 'Exercise does not exist.',
             keyTranslation: 'exercise.does.not.exist',
             details: ['exerciseId' => $exerciseId]
+        );
+    }
+
+    public static function typeIsNotAvailable(string $type): self
+    {
+        return new static(
+            title: 'The exercise type does not exist.',
+            keyTranslation: 'exercise.type.does.not.exist',
+            details: [
+                'type' => $type,
+                'availableTypes' => Exercise::AVAILABLE_TYPES,
+            ]
+        );
+    }
+
+    public static function atLeastOneMuscleGroupRequired(): self
+    {
+        return new static(
+            title: 'The exercise must have at least one muscle group.',
+            keyTranslation: 'exercise.at.least.one.muscle.group.required',
+            details: []
         );
     }
 }
