a9e8f128158fba3b3f17f94b64ba471f42e16a8c
Antobio17 <antoniojr997@gmail.com>
2026-07-09 19:29:43 +0200

    feat: update ThemeProvider to include EnvironmentProviders and improve provider type definitions

--- frontend/src/app/shared/theme/infrastructure/providers/theme.provider.ts
+++ frontend/src/app/shared/theme/infrastructure/providers/theme.provider.ts
@@ -1,10 +1,10 @@
-import { Provider } from "@angular/core";
+import { EnvironmentProviders, Provider, inject, provideAppInitializer } from "@angular/core";
 import { UpdateThemePort } from "../../domain/ports/update-theme.port";
 import { HttpUpdateThemeAdapter } from "../adapters/http-update-theme.adapter";
 import { ThemeService } from "../../application/services/theme.service";
 
 export class ThemeProvider {
-  static getProviders(): Provider[] {
+  static getProviders(): Array<Provider | EnvironmentProviders> {
     return [
       { provide: UpdateThemePort, useClass: HttpUpdateThemeAdapter },
       {
@@ -12,6 +12,7 @@ export class ThemeProvider {
         useFactory: (port: UpdateThemePort) => new ThemeService(port),
         deps: [UpdateThemePort],
       },
+      provideAppInitializer(() => { inject(ThemeService); }),
     ];
   }
 }
